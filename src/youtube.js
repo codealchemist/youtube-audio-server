905e4753f1b72aa021bc8d85fb04d0178d2cf668
Alberto Miranda <amiranda@elementum.com>
2018-10-12 11:49:34 -0300

    Fixed: downloading video file instead of just audio.

--- src/youtube.js
+++ src/youtube.js
@@ -1,72 +1,70 @@
-const fs = require('fs')
-const ytdl = require('ytdl-core')
-const YtNode = require('youtube-node')
-const through2 = require('through2')
-const Ffmpeg = require('fluent-ffmpeg')
+const fs = require("fs");
+const ytdl = require("ytdl-core");
+const YtNode = require("youtube-node");
+const through2 = require("through2");
+const Ffmpeg = require("fluent-ffmpeg");
 
-const apiKey = process.env.KEY
-const ytNode = new YtNode()
-ytNode.setKey(apiKey)
+const apiKey = process.env.KEY;
+const ytNode = new YtNode();
+ytNode.setKey(apiKey);
 
 class YouTube {
-  constructor () {
-    this.pageSize = 10
+  constructor() {
+    this.pageSize = 10;
   }
 
-  stream (id, res) {
-    const video = ytdl(id)
-    const ffmpeg = new Ffmpeg(video)
-    const stream = through2()
+  stream(id) {
+    const video = ytdl(id);
+    const ffmpeg = new Ffmpeg(video);
+    const stream = through2();
 
     try {
-      ffmpeg
-        .format('mp3')
-        .pipe(stream)
+      ffmpeg.format("mp3").pipe(stream);
 
-      return stream
+      return stream;
     } catch (e) {
-      throw e
+      throw e;
     }
   }
 
-  download ({id, file = './youtube-audio.mp3'}, callback) {
-    const url = `//youtube.com/watch?v=${id}`
-    const fileWriter = fs.createWriteStream(file)
+  download({ id, file = "./youtube-audio.mp3" }, callback) {
+    const url = `//youtube.com/watch?v=${id}`;
+    const fileWriter = fs.createWriteStream(file);
 
     try {
-      ytdl(url).pipe(fileWriter)
+      this.stream(id).pipe(fileWriter);
     } catch (e) {
-      throw e
+      throw e;
     }
 
-    fileWriter.on('finish', () => {
-      fileWriter.end()
+    fileWriter.on("finish", () => {
+      fileWriter.end();
 
-      if (typeof callback === 'function') {
-        callback(null, {id, file})
+      if (typeof callback === "function") {
+        callback(null, { id, file });
       }
-    })
+    });
 
-    fileWriter.on('error', (error) => {
-      fileWriter.end()
+    fileWriter.on("error", error => {
+      fileWriter.end();
 
-      if (typeof callback === 'function') {
-        callback(error, null)
+      if (typeof callback === "function") {
+        callback(error, null);
       }
-    })
+    });
   }
 
-  search ({query, page}, callback) {
+  search({ query, page }, callback) {
     if (page) {
-      ytNode.addParam('pageToken', page)
+      ytNode.addParam("pageToken", page);
     }
 
-    ytNode.search(query, this.pageSize, callback)
+    ytNode.search(query, this.pageSize, callback);
   }
 
-  get (id, callback) {
-    ytNode.getById(id, callback)
+  get(id, callback) {
+    ytNode.getById(id, callback);
   }
 }
 
-module.exports = new YouTube()
+module.exports = new YouTube();
